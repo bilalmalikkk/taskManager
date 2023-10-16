24518d9c4e2c5deafdbbee86cd3cae061db9b75c
Bilal Malik <bilalmalik746429@gmail.com>
2023-10-17 02:41:36 +0500

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,33 @@
+// JavaScript code for form validation
+document.getElementById("login-form").onsubmit = function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    // You can add your validation logic here and perform login actions.
+    console.log(`Login with username: ${username} and password: ${password}`);
+    window.location.href = "home.html";
+};
+
+document.getElementById("signup-form").onsubmit = function (e) {
+    e.preventDefault();
+    const newUsername = document.getElementById("new-username").value;
+    const newPassword = document.getElementById("new-password").value;
+    
+    // You can add your validation logic here and perform signup actions.
+    console.log(`Sign up with new username: ${newUsername} and password: ${newPassword}`);
+    window.location.href = "login.html";
+};
+
+// JavaScript code for task manager
+const taskInput = document.getElementById("task");
+const taskList = document.getElementById("task-list");
+
+document.getElementById("add-task").onclick = function () {
+    const taskText = taskInput.value;
+    if (taskText !== "") {
+        const li = document.createElement("li");
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+};
